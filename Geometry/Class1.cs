f9c5f8f27b44d742375d5b91a24f08f7125107b2
leonovich <leonovich.d.a13@gmail.com>
2022-02-20 15:20:58 +0700

    Interface IFigure

--- Geometry/Class1.cs
+++ Geometry/Class1.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Geometry
 {
-    public class Class1
+    public class Vector2
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+    public class Vector3
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double W { get; set; }
+    }
+
+    public class Matrix3
+    {
+        Vector3 First { get; set; }
+        Vector3 Second { get; set; }
+        Vector3 Third { get; set; }
+    }
+
+    public class Transform
+    {
+        public Vector2 Position { get; set; }
+        public double Rotation { get; set; }
+        public Vector2 Scale { get; set; }
+
+        // Matrix3 View { get; }
+        // Matrix3 Model { get; }
+
+    }
+    public interface IFigure
+    {
+        Transform Transform { get; set; } // преобразование локальной системы координат
+        (double left, double top, double right, double bottom) AABB { get; } // контур вокруг фигуры, стороны прямоугольника параллельны осям
+        (double left, double top, double right, double bottom) OBB { get; } // контур вокруг фигуры в локальной системе координат
+        // List<string> FigureAsString { get; } // (может и не надо) возвращает набор строк, перая строка имя фигуры, дальше параметры в зависимости от типа фигуры
+        bool PointInFigure(Vector2 position, double eps); // проверяет, что точка с координатами position внутри фигуры с точностью eps
+        List<Matrix3> Curves { get; } // возвращает кривые 2 порядка для описания фигуры
+        bool FigureIsClosed { get; set; } // замкнута фигура (true) или нет (false)
     }
 }
