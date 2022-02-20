f5582952d9c9630570a317b259f6236a3cae7158
Notrum666 <khandei@mail.ru>
2022-02-20 13:52:41 +0700

    Added all neccesary linear algebra

---
+++ LinearAlgebra/Quaternion.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearAlgebra
+{
+    public enum EulerOrder
+    {
+        XYZ,
+        XZY,
+        YXZ,
+        YZX,
+        ZXY,
+        ZYX
+    }
+    // do NOT ask me about this, i don't give a fuck how this works. (times tried to understand: 2)
+    public struct Quaternion
+    {
+        public double w, x, y, z;
+
+        public static Quaternion Identity { get { return new Quaternion(1); } }
+        public static Quaternion Zero { get { return new Quaternion(); } }
+        public Quaternion(double w = 0, double x = 0, double y = 0, double z = 0)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        /// <summary>
+        /// Dot product
+        /// </summary>
+        public double dot(Quaternion q)
+        {
+            return w * q.w + x * q.x + y * q.y + z * q.z;
+        }
+        /// <summary>
+        /// Norm of quaternion
+        /// </summary>
+        public double norm()
+        {
+            return dot(this);
+        }
+        /// <summary>
+        /// Length of quaternion. Same as magnitude
+        /// </summary>
+        public double length()
+        {
+            return Math.Sqrt(norm());
+        }
+        /// <summary>
+        /// Magnitude of quaternion. Same as length
+        /// </summary>
+        public double magnitude()
+        {
+            return length();
+        }
+        /// <summary>
+        /// Checks if quaternion small enough to be considered a zero quaternion
+        /// </summary>
+        public bool isZero()
+        {
+            return norm() < Constants.SqrEpsilon;
+        }
+        /// <summary>
+        /// Normalizes this quaternion
+        /// </summary>
+        public void normalize()
+        {
+            double l = length();
+            w /= l;
+            x /= l;
+            y /= l;
+            z /= l;
+        }
+        /// <summary>
+        /// Returns normalized copy of this quaternion
+        /// </summary>
+        public Quaternion normalized()
+        {
+            return this / length();
+        }
+        /// <summary>
+        /// Conjugates this quaternion
+        /// </summary>
+        public void conjugate()
+        {
+            x = -x;
+            y = -y;
+            z = -z;
+        }
+        /// <summary>
+        /// Returns conjugated copy of this quaternion
+        /// </summary>
+        public Quaternion conjugated()
+        {
+            return new Quaternion(w, -x, -y, -z);
+        }
+        /// <summary>
+        /// Cross product
+        /// </summary>
+        public Quaternion cross(Quaternion q)
+        {
+            return new Quaternion(0.0, y * q.z - z * q.y, z * q.x - x * q.z, x * q.y - y * q.x);
+        }
+        /// <summary>
+        /// Checks if quaternions are parallel enough to be considered collinear
+        /// </summary>
+        /// <returns>True if vectors are collinear, false otherwise</returns>
+        public bool isCollinearTo(Quaternion q)
+        {
+            return cross(q).isZero();
+        }
+        /// <summary>
+        /// Returns inverse copy of this quaternion
+        /// </summary>
+        public Quaternion inverse()
+        {
+            return conjugated() / norm();
+        }
+        /// <summary>
+        /// Inverts this quaternion
+        /// </summary>
+        public void invert()
+        {
+            double n = norm();
+            conjugate();
+            w /= n;
+            x /= n;
+            y /= n;
+            z /= n;
+        }
+        /// <summary>
+        /// Creates quaternion that represents rotation around axis for angle
+        /// </summary>
+        public static Quaternion FromAxisAngle(Vector3 axis, double angle)
+        {
+            if (Math.Abs(axis.squaredLength() - 1.0) > Constants.SqrEpsilon)
+                throw new ArgumentException("Axis is not normalized.");
+            double sinhalf = Math.Sin(angle / 2.0);
+            return new Quaternion(Math.Cos(angle / 2.0), axis.x * sinhalf, axis.y * sinhalf, axis.z * sinhalf);
+        }
+        /// <summary>
+        /// Creates quaternion from euler angles in specified order
+        /// </summary>
+        public static Quaternion FromEuler(Vector3 euler, EulerOrder order = EulerOrder.ZXY)
+        {
+            double sinhalfx = Math.Sin(euler.x / 2.0);
+            double sinhalfy = Math.Sin(euler.y / 2.0);
+            double sinhalfz = Math.Sin(euler.z / 2.0);
+            double coshalfx = Math.Cos(euler.x / 2.0);
+            double coshalfy = Math.Cos(euler.y / 2.0);
+            double coshalfz = Math.Cos(euler.z / 2.0);
+            switch (order)
+            {
+                case EulerOrder.XYZ:
+                    return new Quaternion(coshalfz, 0.0, 0.0, sinhalfz) *
+                           new Quaternion(coshalfy, 0.0, sinhalfy, 0.0) *
+                           new Quaternion(coshalfx, sinhalfx, 0.0, 0.0);
+                case EulerOrder.XZY:
+                    return new Quaternion(coshalfy, 0.0, sinhalfy, 0.0) *
+                           new Quaternion(coshalfz, 0.0, 0.0, sinhalfz) *
+                           new Quaternion(coshalfx, sinhalfx, 0.0, 0.0);
+                case EulerOrder.YXZ:
+                    return new Quaternion(coshalfz, 0.0, 0.0, sinhalfz) *
+                           new Quaternion(coshalfx, sinhalfx, 0.0, 0.0) *
+                           new Quaternion(coshalfy, 0.0, sinhalfy, 0.0);
+                case EulerOrder.YZX:
+                    return new Quaternion(coshalfx, sinhalfx, 0.0, 0.0) *
+                           new Quaternion(coshalfz, 0.0, 0.0, sinhalfz) *
+                           new Quaternion(coshalfy, 0.0, sinhalfy, 0.0);
+                case EulerOrder.ZXY:
+                    return new Quaternion(coshalfy, 0.0, sinhalfy, 0.0) *
+                           new Quaternion(coshalfx, sinhalfx, 0.0, 0.0) *
+                           new Quaternion(coshalfz, 0.0, 0.0, sinhalfz);
+                case EulerOrder.ZYX:
+                    return new Quaternion(coshalfx, sinhalfx, 0.0, 0.0) *
+                           new Quaternion(coshalfy, 0.0, sinhalfy, 0.0) *
+                           new Quaternion(coshalfz, 0.0, 0.0, sinhalfz);
+            }
+            throw new NotImplementedException();
+        }
+
+        public static Quaternion operator /(Quaternion lhs, double rhs)
+        {
+            return new Quaternion(lhs.w / rhs, lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
+        }
+        public static Quaternion operator *(Quaternion lhs, double rhs)
+        {
+            return new Quaternion(lhs.w * rhs, lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
+        }
+        public static Quaternion operator *(double lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs * rhs.w, lhs * rhs.x, lhs * rhs.y, lhs * rhs.z);
+        }
+        public static Quaternion operator *(Quaternion lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z,
+                                  lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y,
+                                  lhs.w * rhs.y - lhs.x * rhs.z + lhs.y * rhs.w + lhs.z * rhs.x,
+                                  lhs.w * rhs.z + lhs.x * rhs.y - lhs.y * rhs.x + lhs.z * rhs.w);
+        }
+        public static Vector3 operator *(Quaternion lhs, Vector3 rhs)
+        {
+            Quaternion q = lhs * new Quaternion(0.0, rhs.x, rhs.y, rhs.z) * lhs.inverse();
+            return new Vector3(q.x, q.y, q.z);
+        }
+        public static Quaternion operator +(Quaternion lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs.w + rhs.w, lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+        public static Quaternion operator -(Quaternion lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs.w - rhs.w, lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        }
+
+        //public static Quaternion operator *(Quaternion q, Vector3 v)
+        //{
+        //    return new Quaternion(-q.x * v.x - q.y - v.y - q.z * v.z,
+        //                          q.w * v.x + q.y * v.z - q.z * v.y,
+        //                          q.w * v.y + q.x * v.z - q.z * v.x,
+        //                          q.w * v.z + q.x * v.y - q.y * v.x);
+        //}
+        ///// <summary>
+        ///// Rotates vector v by quaternion q
+        ///// </summary>
+        //public Vector3 rotateVector(Vector3 vec)
+        //{
+        //    Quaternion result = this * vec * inversed();
+        //    return new Vector3(result.x, result.y, result.z);
+        //}
+        ///// <summary>
+        ///// Returns eulers representation of rotation in this quaternion
+        ///// </summary>
+        //public Vector3 toEuler()
+        //{
+        //    if (x * y + z * w == 0.5)
+        //        return new Vector3(Math.Asin(2 * x * y + 2 * z * w), 2 * Math.Atan2(x, w), 0);
+        //    else
+        //    if (x * y + z * w == -0.5)
+        //        return new Vector3(Math.Asin(2 * x * y + 2 * z * w), -2 * Math.Atan2(x, w), 0);
+        //    else
+        //        return new Vector3(Math.Asin(2 * x * y + 2 * z * w), Math.Atan2(2 * y * w - 2 * x * z, 1 - 2 * y * y - 2 * z * z), Math.Atan2(2 * x * w - 2 * y * z, 1 - 2 * x * x - 2 * z * z));
+        //}
+        public override string ToString()
+        {
+            return "(" + w.ToString() + ", " + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ")";
+        }
+        public string ToString(string format)
+        {
+            return "(" + w.ToString(format) + ", " + x.ToString(format) + ", " + y.ToString(format) + ", " + z.ToString(format) + ")";
+        }
+    }
+}
---
+++ LinearAlgebra/Vector4f.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearAlgebra
+{
+    /// <summary>
+    /// 4-dimensional vector with single precision
+    /// </summary>
+    public struct Vector4f
+    {
+        /// <summary>
+        /// Returns new zero vector
+        /// </summary>
+        public static readonly Vector4f Zero = new Vector4f();
+        /// <summary>
+        /// Returns new unit x vector (x = 1, y = 0, z = 0, w = 0)
+        /// </summary>
+        public static readonly Vector4f UnitX = new Vector4f(1f, 0f, 0f, 0f);
+        /// <summary>
+        /// Returns new unit y vector (x = 0, y = 1, z = 0, w = 0)
+        /// </summary>
+        public static readonly Vector4f UnitY = new Vector4f(0f, 1f, 0f, 0f);
+        /// <summary>
+        /// Returns new unit z vector (x = 0, y = 0, z = 1, w = 0)
+        /// </summary>
+        public static readonly Vector4f UnitZ = new Vector4f(0f, 0f, 1f, 0f);
+        /// <summary>
+        /// Returns new unit w vector (x = 0, y = 0, z = 0, w = 1)
+        /// </summary>
+        public static readonly Vector4f UnitW = new Vector4f(0f, 0f, 0f, 1f);
+
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public float w { get; set; }
+        public Vector3f xyz { get { return new Vector3f(x, y, z); } }
+        public Vector4f(float x = 0f, float y = 0f, float z = 0f, float w = 0f)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4f(Vector2f vec, float z = 0f, float w = 0f)
+        {
+            x = vec.x;
+            y = vec.y;
+            this.z = z;
+            this.w = w;
+        }
+        public Vector4f(Vector3f vec, float w = 0f)
+        {
+            x = vec.x;
+            y = vec.y;
+            z = vec.z;
+            this.w = w;
+        }
+        public Vector4f(params float[] values)
+        {
+            if (values.Length != 4)
+                throw new Exception("Array length must be 4.");
+
+            x = values[0];
+            y = values[1];
+            z = values[2];
+            w = values[3];
+        }
+
+        public static explicit operator Vector4f(Vector4 vec) => new Vector4f((float)vec.x, (float)vec.y, (float)vec.z, (float)vec.w);
+
+        /// <summary>
+        /// Magnitude of vector. Same as length
+        /// </summary>
+        public float magnitude()
+        {
+            return (float)Math.Sqrt(squaredMagnitude());
+        }
+        /// <summary>
+        /// Magnitude of vector without root. Same as squaredLength
+        /// </summary>
+        public float squaredMagnitude()
+        {
+            return dotMul(this);
+        }
+        /// <summary>
+        /// Length of vector. Same as length
+        /// </summary>
+        public float length()
+        {
+            return magnitude();
+        }
+        /// <summary>
+        /// Length of vector without root. Same as squaredMagnitude
+        /// </summary>
+        public float squaredLength()
+        {
+            return squaredMagnitude();
+        }
+        /// <summary>
+        /// Checks if vector small enough to be considered a zero vector
+        /// </summary>
+        public bool isZero()
+        {
+            return squaredMagnitude() < Constants.SqrFloatEpsilon;
+        }
+        public static Vector4f operator +(Vector4f v1, Vector4f v2)
+        {
+            return new Vector4f(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w);
+        }
+        public static Vector4f operator -(Vector4f v1, Vector4f v2)
+        {
+            return new Vector4f(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w);
+        }
+        public static Vector4f operator *(Vector4f vec, float value)
+        {
+            return new Vector4f(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4f operator *(float value, Vector4f vec)
+        {
+            return new Vector4f(vec.x * value, vec.y * value, vec.z * value, vec.w * value);
+        }
+        public static Vector4f operator /(Vector4f vec, float value)
+        {
+            return new Vector4f(vec.x / value, vec.y / value, vec.z / value, vec.w / value);
+        }
+        public static Vector4f operator -(Vector4f vec)
+        {
+            return new Vector4f(-vec.x, -vec.y, -vec.z, -vec.w);
+        }
+        /// <summary>
+        /// Dot product
+        /// </summary>
+        public static float operator *(Vector4f v1, Vector4f v2)
+        {
+            return v1.dotMul(v2);
+        }
+        /// <summary>
+        /// Dot product
+        /// </summary>
+        public float dotMul(Vector4f vec)
+        {
+            return x * vec.x + y * vec.y + z * vec.z + w * vec.w;
+        }
+        /// <summary>
+        /// Component multiplication
+        /// </summary>
+        /// <returns>New vector: (x1*x2, y1*y2, z1*z2, w1*w2)</returns>
+        public Vector4f compMul(Vector4f vec)
+        {
+            return new Vector4f(x * vec.x, y * vec.y, z * vec.z, w * vec.w);
+        }
+        /// <summary>
+        /// Component division
+        /// </summary>
+        /// <returns>New vector: (x1/x2, y1/y2, z1/z2, w1/w2)</returns>
+        public Vector4f compDiv(Vector4f vec)
+        {
+            return new Vector4f(x / vec.x, y / vec.y, z / vec.z, w / vec.w);
+        }
+        /// <summary>
+        /// Returns normalized copy of this vector
+        /// </summary>
+        public Vector4f normalized()
+        {
+            return this / magnitude();
+        }
+        /// <summary>
+        /// Normalizes this vector
+        /// </summary>
+        public void normalize()
+        {
+            float magn = magnitude();
+            x /= magn;
+            y /= magn;
+            z /= magn;
+            w /= magn;
+        }
+        /// <summary>
+        /// Checks if vectors are equal enough to be considered equal
+        /// </summary>
+        public bool equals(Vector4f vec)
+        {
+            return (vec - this).isZero();
+        }
+        /// <summary>
+        /// Projects vector on another vector
+        /// </summary>
+        public Vector4f projectOnVector(Vector4f vec)
+        {
+            if (vec.isZero())
+                return Vector4f.Zero;
+            return vec * (this * vec / vec.squaredMagnitude());
+        }
+        /// <summary>
+        /// Projects vector on flat
+        /// </summary>
+        /// <param name="flatNorm">Normal vector to flat (not necessary normalized)</param>
+        /// <returns></returns>
+        public Vector4f projectOnFlat(Vector4f flatNorm)
+        {
+            return this - flatNorm * (this * flatNorm / flatNorm.squaredMagnitude());
+        }
+        public override string ToString()
+        {
+            return "(" + x.ToString() + ", " + y.ToString() + ", " + z.ToString() + ", " + w.ToString() + ")";
+        }
+        public string ToString(string format)
+        {
+            return "(" + x.ToString(format) + ", " + y.ToString(format) + ", " + z.ToString(format) + ", " + w.ToString(format) + ")";
+        }
+    }
+}
