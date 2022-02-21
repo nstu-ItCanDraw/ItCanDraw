a80498ea5fafe3b9aa6c5da2ab28970d7f8ace8c
sysoev.2018 <sysoev.2018@CLOUD-RDHOST1>
2022-02-21 18:24:27 +0700

    Создание подмодулей экспорта

---
+++ IO/Converters/HTML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    public static class HTML
+    {
+        public static string GetHTMLStringFromSVGString(string svgString)
+        {
+            return "<html><head><title>Hahahahha</title></head><body><?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xml=\"http://www.w3.org/XML/1998/namespace\" width=\"500\" height=\"500\" viewBox=\"-250, -250, 500, 500\" style=\"fill:blue;\">  <g>    <rect x=\"100\" y=\"100\" width=\"200\" height=\"50\" transform=\"rotate(45, 0, 0)\" style=\"fill:red;\" />  </g></svg></body></html>";
+        }
+    }
+}
