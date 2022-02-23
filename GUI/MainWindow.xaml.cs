c37962ae48d257a982d10aeae9d4fa35e4e6e176
Notrum666 <khandei@mail.ru>
2022-02-23 17:10:04 +0700

    Added RenderViewModel, base opengl initialization and rendering

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        // commands don't work properly with GLWpfControl, so all viewmodel commands need to be triggered manually
+        private RenderViewModel renderViewModel = new RenderViewModel();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenTKControl_Render(TimeSpan obj)
+        {
+            if (renderViewModel.UpdateCommand.CanExecute(obj))
+                renderViewModel.UpdateCommand.Execute(obj);
+        }
+
+        private void OpenTKControl_Initialized(object sender, EventArgs e)
+        {
+            OpenTKControl.DataContext = renderViewModel;
+
+            if (renderViewModel.InitializedCommand.CanExecute(OpenTKControl))
+                renderViewModel.InitializedCommand.Execute(OpenTKControl);
         }
     }
 }
