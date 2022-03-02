0e10401bf5c6b2e8d70495f4fde107d7f5a68812
Hrenaki <shikhovwtr@mail.ru>
2022-03-02 19:42:34 +0700

    'Polygone' was renamed to 'Polygon'

--- Geometry/Polygon.cs
+++ Geometry/Polygon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using LinearAlgebra;
@@ -11,7 +12,7 @@ namespace Geometry
     {
         List<Vector2> Points { get; set; }
     }
-    class Polygone : NotifyPropertyChanged, IPolygone
+    class Polygon : NotifyPropertyChanged, IPolygone
     {
         public List<Vector2> Points { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
