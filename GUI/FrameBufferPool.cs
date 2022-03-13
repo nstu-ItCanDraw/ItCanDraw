3eec762c36a8032fc900b062a02eab188185ae13
IO256 <IO256@DESKTOP-C2OQE9L>
2022-03-13 16:19:10 +0700

    Removed useless variables, remade FrameBufferPool class

--- GUI/FrameBufferPool.cs
+++ GUI/FrameBufferPool.cs
@@ -6,17 +6,8 @@ namespace GUI
 {
     internal class FrameBufferPool
     {       
-        private int _maxCapacity;
-        private int _frameBufferAmmount;
         private List<FrameBuffer> inUseFrameBuffers = new List<FrameBuffer>();
         private Stack<FrameBuffer> freeFrameBuffers = new Stack<FrameBuffer>();
-        public int FrameBufferAmmount
-        {
-            get
-            {
-                return inUseFrameBuffers.Count;
-            }
-        }
 
         public int Width { get; private set; }
         public int Height { get; private set; }
@@ -25,12 +16,9 @@ namespace GUI
         public TextureMinFilter MinFilter { get; private set; }
         public TextureMagFilter MagFilter { get; private set; }
 
-        public FrameBufferPool(int frameBufferAmmount, int maxCapacity, int width, int height, TextureType textureType = TextureType.RGBAColor, TextureWrapMode wrapMode = TextureWrapMode.Repeat,
+        public FrameBufferPool(int frameBufferAmmount, int width, int height, TextureType textureType = TextureType.RGBAColor, TextureWrapMode wrapMode = TextureWrapMode.Repeat,
             TextureMinFilter minFilter = TextureMinFilter.Nearest, TextureMagFilter magFilter = TextureMagFilter.Nearest)
         {
-            _frameBufferAmmount = frameBufferAmmount;
-            _maxCapacity = maxCapacity;
-
             Width = width;
             Height = height;
             TextureType = textureType;
@@ -38,23 +26,24 @@ namespace GUI
             MinFilter = minFilter;
             MagFilter = magFilter;
 
-            for (int i = 0; i < _frameBufferAmmount; i++)
+            for (int i = 0; i < frameBufferAmmount; i++)
+            { 
                 inUseFrameBuffers.Add(new FrameBuffer(new Texture2D(Width, Height, TextureType, WrapMode, MinFilter, MagFilter)));
-
-            for (int i = _frameBufferAmmount; i < _maxCapacity; i++)
-                freeFrameBuffers.Push(new FrameBuffer(new Texture2D(Width, Height, TextureType, WrapMode, MinFilter, MagFilter)));
+                freeFrameBuffers.Push(inUseFrameBuffers[i]);
+            }
         }
         public FrameBuffer PoolGetFreeFrameBuffer()
         {
-            if (FrameBufferAmmount > _maxCapacity)
+            if (freeFrameBuffers.Count == 0)
             {
-                _maxCapacity *= 2;
-                for (int i = _frameBufferAmmount; i < _maxCapacity; i++)
-                    freeFrameBuffers.Push(new FrameBuffer(new Texture2D(Width, Height, TextureType, WrapMode, MinFilter, MagFilter)));
+                for (int i = 0; i < inUseFrameBuffers.Count; i++)
+                {
+                    inUseFrameBuffers.Add(new FrameBuffer(new Texture2D(Width, Height, TextureType, WrapMode, MinFilter, MagFilter)));
+                    freeFrameBuffers.Push(inUseFrameBuffers[i]);
+                }
             }
 
-            inUseFrameBuffers.Add(freeFrameBuffers.Pop());
-            return inUseFrameBuffers[^1];
+            return freeFrameBuffers.Pop();
         }
         public void PoolReleaseFrameBuffer(FrameBuffer curFrameBuffer)
         {
@@ -62,14 +51,12 @@ namespace GUI
             foreach (FrameBuffer fb in inUseFrameBuffers)
                 if (fb == curFrameBuffer)
                 {
+                    freeFrameBuffers.Push(curFrameBuffer);
                     exists = true;
                     break;
                 }
             if (!exists)
                 throw new ArgumentException("This FrameBuffer does not exist in FrameBufferPool");
-
-            inUseFrameBuffers.Remove(curFrameBuffer);
-            freeFrameBuffers.Push(curFrameBuffer);
         }
     }
 }
