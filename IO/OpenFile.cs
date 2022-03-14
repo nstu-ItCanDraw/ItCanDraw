bf948bf99f3b1843deec21c0dd6a85f1eb2a88a8
Vyacheslav Sysoev <sys.viacheslav.oev@gmail.com>
2022-03-14 07:46:23 +0700

    Add ExportFile, OpenFile, SaveFile classes

---
+++ IO/OpenFile.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+using Newtonsoft.Json;
+
+namespace IO
+{
+    public static class OpenFile
+    {
+        public static object FromJSON(string filename)
+        {
+            string json_string;
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                json_string = reader.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<object>(json_string);
+        }
+    }
+}
---
+++ IO/SaveFile.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+using Newtonsoft.Json;
+
+namespace IO
+{
+    public static class SaveFile
+    {
+        public static void ToJSON(string filename, object document)
+        {
+            string json_string = JsonConvert.SerializeObject(document);
+
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.Write(json_string);
+            }
+        }
+    }
+}
