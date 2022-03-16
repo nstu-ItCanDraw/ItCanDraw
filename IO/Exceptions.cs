cff83d8452228c4201583d06f19e0edf17a41e57
kasimov.2018 <kasimov.2018@stud.nstu.ru>
2022-03-16 19:45:46 +0700

    Init comit

---
+++ IO/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    [Serializable]
+    public class BadFileExtensionError : Exception
+    {
+        public BadFileExtensionError(string message) : base(message)
+        { }
+
+        protected BadFileExtensionError(SerializationInfo info, StreamingContext ctxt) : base(info, ctxt)
+        { }
+    }
+
+    [Serializable]
+    public class BadFileError : Exception
+    {
+        public BadFileError(string message) : base(message)
+        { }
+
+        protected BadFileError(SerializationInfo info, StreamingContext ctxt) : base(info, ctxt)
+        { }
+    }
+}
