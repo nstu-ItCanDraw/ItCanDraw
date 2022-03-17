a3c2474a483b7946d7380feb881700ffb53b272a
leonovich <leonovich.d.a13@gmail.com>
2022-03-17 20:47:54 +0700

    delete usings from gui (sorry for many commits because I inattentive)

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Geometry;
-using LinearAlgebra;
 
 namespace GUI
 {
