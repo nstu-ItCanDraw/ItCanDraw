3b23aba2d6bb487bfe1dcec1bd2e0b487037e2c0
Hrenaki <shikhovwtr@mail.ru>
2022-03-20 22:20:07 +0700

    Project was structured

---
+++ Geometry/Operators/IOperator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinearAlgebra;
+
+namespace Geometry.Operators
+{
+    public enum OperatorType
+    {
+        Union,
+        Intersection,
+        Exclusion
+    }
+
+    public interface IOperator : IGeometry
+    {
+        OperatorType Type { get; } // тип оператора
+
+        IReadOnlyCollection<IGeometry> Operands { get; } // возвращает набор фигур операндов
+
+        void AddOperand(IGeometry operand); // добавляет операнд в конец
+        void InsertOperand(int index, IGeometry operand); // вставляет операнд по индексу
+
+        void RemoveOperand(IGeometry operand); // убирает существующий операнд
+        void RemoveOperandAt(int index); // убирает операнд по индексу
+
+        void MakeOperandFirst(IGeometry operand); // делает существующий операнд первым
+        void MakeOperandLast(IGeometry operand); // делает существующий операнд последним
+        void ReplaceOperandTo(int index, IGeometry operand); // перемещает существующий операнд на место {index}
+
+        IReadOnlyCollection<Vector2> BasicPoints { get; }
+    }
+}
