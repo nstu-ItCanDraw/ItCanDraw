54287a82a885a6c40b00d0222957098a66bef295
Albina09 <99961024+Albina09@users.noreply.github.com>
2022-03-24 10:38:38 +0700

    IO tests

---
+++ IOIO.Test/SaveFileTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IO.Test
+{
+    [TestClass()]
+    public class SaveFileTest
+    {
+        [TestMethod()]
+        public void testSaveToJSON()
+        {
+            string filename = @"..\..\..\Test1.json";
+            Logic.IDocument document = Logic.DocumentFactory.CreateDocument("JSON", 100, 100);
+
+            SaveFile.ToJSON(filename, document);
+
+        }
+    }
+}
